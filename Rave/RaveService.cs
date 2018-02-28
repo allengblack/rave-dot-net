bfb0aea7469ea7a79054583ea6ea3e4d782dcb9e
Ikechi Michael <mykehell123@gmail.com>
2018-02-28 17:33:18 +0100

    feat: add RequeryTransaction method

--- Rave/RaveService.cs
+++ Rave/RaveService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using Rave.Events;
 using Rave.Models;
+using Rave.Models.Events;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Rave.Helpers;
 
 namespace Rave
 {
@@ -13,6 +16,7 @@ namespace Rave
         public string IntegrityHash { get;  set; }
         protected ConfigModel Config { get; set; }
         protected PaymentRequestModel Request { get; set; }
+        protected dynamic TransactionData { get; set; }
 
         protected SHA256Managed hash;
 
@@ -21,7 +25,7 @@ namespace Rave
             this.Request = request;
             this.hash = new SHA256Managed();
         }
-        
+
         public string CreateCheckSum() {
             StringBuilder sb = new StringBuilder();
             sb.Append(this.Config.PublicKey);
@@ -48,6 +52,57 @@ namespace Rave
             byte[] transformedBytes = hash.ComputeHash(bytes);
             
             return this.IntegrityHash = Encoding.UTF8.GetString(transformedBytes);
+        }
+
+        public async Task<ResponseModel<PaymentResponseModel>> RequeryTransaction(string transactionReference = null) {
+            var client = ApiClient.GetApiClient();
+            this.RequeryCount++;
+            transactionReference = transactionReference ?? this.Request.TransactionReference;
+            this.OnRequery(new RequeryEventArgs() {
+                TransactionReference = transactionReference
+            });
+            var body = new {
+                txref = transactionReference,
+                SECKEY = this.Config.SecretKey,
+                last_attempt = 1
+            };
+            
+            try {
+                var result = await client.Post<ResponseModel<PaymentResponseModel>>(this.Config.GetUrl("/flwv3-pug/getpaidx/api/xrequery"), ApiClient.GetJsonContent<object>(body));
+
+                if (result.IsSuccessful()) {
+                    this.OnSuccess(new SuccessEventArgs() {
+                        Request = this.Request,
+                        Response = result
+                    });
+                }
+                else if (result.IsFailed()) {
+                    this.OnFailed(new FailedEventArgs() {
+                        Request = this.Request,
+                        Response = result
+                    });
+                }
+                else {
+                    if (this.RequeryCount >= 4) {
+                        this.OnTimeout(new TimeoutEventArgs() {
+                            Request = this.Request,
+                            Response = result
+                        });
+                    }
+                    else {
+                        System.Threading.Thread.Sleep(3000); //wait for 3 seconds before retrying
+                        return await this.RequeryTransaction(transactionReference);
+                    }
+
+                }
+                return result;
+            }
+            catch (Exception ex) {
+                this.OnRequeryError(new RequeryErrorEventArgs() {
+                    Error = ex
+                });
+            }
+            return null;
         }
     }
 }
